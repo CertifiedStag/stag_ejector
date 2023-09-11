9e2c0552d892eda7eff19366a479b60fabffd4cd
CertifiedStag <107716735+CertifiedStag@users.noreply.github.com>
2023-09-11 18:53:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,8 @@ lua54 'yes'
 author 'CertifiedStag'
 name 'Ejector Seat'
 description 'A simple Ejector Seat Script'
-github 'https://github.com/CertifiedStag/cs_ejectorseat'
-version '2.0'
+github 'https://github.com/CertifiedStag/cs_ejector'
+version '1.0'
 
 client_scripts {
 	'config.lua',
