0ed4e1a007fcfd78f2f9e0d658b33ab0dc8c5bd5
CertifiedStag <107716735+CertifiedStag@users.noreply.github.com>
2023-09-19 00:58:48 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,20 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local version = "1.0"
+local version = "2.0"
 print("[EjectorSeat] Script v" .. version .. " loaded.")
 
 local ejectorSeatActive = false
 local canEject = true
+
+RegisterKeyMapping("eject", "Eject From Vehicle", "keyboard", "C")
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if
+        IsControlJustReleased(0, Config.KeyToTriggerEject) then
+            TriggerEvent("eject")
+        end
+    end
+end)
 
 RegisterCommand("eject", function()
     if not Config.EnableEjectorSeat then
@@ -19,25 +30,26 @@ RegisterCommand("eject", function()
                 ejectorSeatActive = true
                 local playerCoords = GetEntityCoords(playerPed)
                 local forwardVector = GetEntityForwardVector(vehicle)
-                local ejectPosition = playerCoords + vector3(0.0, 0.0, 0.1) + forwardVector * Config.EjectionDistanceMultiplier
+                local ejectPosition = playerCoords + vector3(0.0, 0.0, 0.0) + forwardVector * Config.EjectionDistanceMultiplier
 
                 local forceMagnitude = 100 -- Adjust the force value as needed
 
                 local ejectPos = playerCoords + vector3(0.0, 0.0, 0.0) + forwardVector * 3.0
                 SetEntityCoordsNoOffset(playerPed, playerCoords.x, playerCoords.y, playerCoords.z + 3, true, true, true)
                 SetEntityCollision(playerPed, true, true)
+                AddExplosion(0, 0, 0, 32, 0, true, false, 100)
                 SetEntityHasGravity(playerPed, false)
                 SetPedToRagdoll(playerPed, 1000, 1000, 0, 0, 0, 0)
                 SetEntityVelocity(playerPed, 30, 30, 30.0)
-                ejectorSeatActive = false
-                
+               ejectorSeatActive = false
+
                 TaskParachute(playerPed, true)
 
                 canEject = false
                 Citizen.SetTimeout(Config.EjectCooldown * 1000, function()
                     canEject = true
                 end)
-
+    
                 QBCore.Functions.Notify("Ejected from the vehicle.", "success")
             else
                 QBCore.Functions.Notify("Ejector seat is already active.", "error")
@@ -49,12 +61,4 @@ RegisterCommand("eject", function()
         QBCore.Functions.Notify("You must have an ejector seat fitted to eject.", "error")
     end
 end)
-RegisterKeyMapping("eject", "Ejector Seat", "keyboard", "q")
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if IsControlJustReleased(0, Config.KeyToTriggerEject) then
-            TriggerEvent("eject")
-        end
-    end
-end)
+
