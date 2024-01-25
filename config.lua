2a30013e9795bbc42e6c661354d76dc410da63be
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-01-25 08:31:22 -0500

    Update config.lua
    adding parachute to config

--- config.lua
+++ config.lua
@@ -4,4 +4,6 @@ Config.EnableEjectorSeat = true -- Set to false to disable ejector seat function
 
 Config.EjectionDistanceMultiplier = 1.0 -- Multiplier for the ejection distance
 
-Config.EjectCooldown = 5 -- Cooldown time in seconds between ejections
+Config.EjectCooldown = 60 -- Cooldown time in seconds between ejections
+
+Config.ParachuteItem = "parachute" -- Name of the parachute item in the inventory
